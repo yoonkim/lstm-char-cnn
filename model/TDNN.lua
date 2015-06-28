aa5de7544df778c0fdf563ba9f1e8927574639a5
ASD <yoon.r.kim@gmail.com>
2015-06-28 14:25:55 -0700

    refactored into main

--- model/TDNN.lua
+++ model/TDNN.lua
@@ -1,24 +1,31 @@
 --[[
- Time-delayed CNN (i.e. 1-d CNN) with multiple filter widths
+ Time-delayed Neural Network (i.e. 1-d CNN) with multiple filter widths
 --]]
 local TDNN = {}
 
-function TDNN.tdnn(length, input_size, output_size, kernels)
-  -- length = length of sentences/words (zero padded to be of same length)
-  -- input_size = embedding_size
-  -- output_size = number of output feature maps (per kernel)
-  -- kernels = table of kernel widths
-  local input = nn.Identity()() --input is batch_size x length x input_size
-  local layer1 = {}
-  for i = 1, #kernels do
-      local reduced_l = length - kernels[i] + 1 
-      local conv_layer = nn.TemporalConvolution(input_size, output_size, kernels[i])(input)
-      local pool_layer = nn.TemporalMaxPooling(reduced_l)(nn.ReLU()(conv_layer))
-      table.insert(layer1, pool_layer)
-  end
-  local layer1_concat = nn.JoinTable(3)(layer1)
-  local output = nn.Squeeze()(layer1_concat)
-  return nn.gModule({input}, {output})
+function TDNN.tdnn(length, input_size, feature_maps, kernels)
+    -- length = length of sentences/words (zero padded to be of same length)
+    -- input_size = embedding_size
+    -- feature_maps = table of feature maps (for each kernel width)
+    -- kernels = table of kernel widths
+    local layer1_concat, output
+    local input = nn.Identity()() --input is batch_size x length x input_size
+    local layer1 = {}
+    for i = 1, #kernels do
+	local reduced_l = length - kernels[i] + 1 
+	local conv = nn.TemporalConvolution(input_size, feature_maps[i], kernels[i])
+	conv.name = 'conv_filter_' .. kernels[i] .. '_' .. feature_maps[i]
+	local conv_layer = conv(input)
+	local pool_layer = nn.TemporalMaxPooling(reduced_l)(nn.Tanh()(conv_layer))
+	table.insert(layer1, pool_layer)
+    end
+    if #kernels > 1 then
+	layer1_concat = nn.JoinTable(3)(layer1)
+	output = nn.Squeeze()(layer1_concat)
+    else
+        output = nn.Squeeze()(pool_layer)    
+    end
+    return nn.gModule({input}, {output})
 end
 
 return TDNN
