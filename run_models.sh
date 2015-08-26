e3f0f10a1e8609a3f41e3051a45bbb079ccb6d44
Yoon Kim <yoon.r.kim@gmail.com>
2015-08-25 21:22:28 -0400

    Update run_models.sh

--- run_models.sh
+++ run_models.sh
@@ -18,7 +18,7 @@ th main.lua -data_dir data/ptb -savefile ptb-word-large -EOS '+' -rnn_size 650 -
 th main.lua -data_dir data/ptb -savefile ptb-char-large -EOS '+' -rnn_size 650 -use_chars 1 -use_words 0 
 -char_vec_size 15 -highway_layers 2 -kernels '{1,2,3,4,5,6,7}' -feature_maps '{50,100,150,200,200,200,200}'
 
-#To reproduce Table 4/Table 5, run the same scripts as above but change data_dir/savefile, 
+#To reproduce Table 4, run the same scripts as above but change data_dir/savefile, 
 #and remove -EOS. So for German (DE), use the following scripts
 #LSTM-Word-Small
 th main.lua -data_dir data/de -savefile de-word-small -rnn_size 200 -use_chars 0 
